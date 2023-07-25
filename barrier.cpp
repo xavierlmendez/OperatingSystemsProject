29e46600bc793f2244996506176c47a25329dee1
Xavier Mendez <44298432+xavierlmendez@users.noreply.github.com>
2023-07-24 19:21:12 -0500

    final code

--- barrier.cpp
+++ barrier.cpp
@@ -6,7 +6,6 @@ using namespace synchronization;
 
    
 barrier::barrier( int numberOfThreads ) {
-   std::cout << "starting barrier constructor\n";
    waiting = 0;
    numOfProcess = numberOfThreads;
    sem_init(&Lock, 0, 1);
@@ -22,24 +21,17 @@ barrier::~barrier( ) {
 
 void barrier::arriveAndWait( void ) {
    sem_wait(&Lock);
-   std::cout << "incrementing wait\n";
    waiting++;
    if(waiting == numOfProcess){
       sem_wait(&Gate2);
       sem_post(&Gate1);
    }
-   std::cout << "1\n";
    sem_post(&Lock);
-   std::cout << "2\n";
    sem_wait(&Gate1);
-   std::cout << "3\n";
    sem_post(&Gate1);
-   std::cout << "4\n";
    
    sem_wait(&Lock);
-   std::cout<<"decrementing wait\n";
    waiting--;
-   std::cout<<"waiting equals "<< waiting<< "\n";
    
    if(waiting == 0 ){
       sem_post(&Gate2);
